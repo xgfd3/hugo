1adc125595c3993bfeb62ebfb098eb869cb8b927
xucz <xucom786647379@gmail.com>
2020-08-26 16:19:15 +0800

    gradle版本升级，配置优化等

--- hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
+++ hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
@@ -25,13 +25,6 @@ class HugoPlugin implements Plugin<Project> {
       variants = project.android.libraryVariants
     }
 
-    project.dependencies {
-      debugCompile 'com.jakewharton.hugo:hugo-runtime:1.2.2-SNAPSHOT'
-      // TODO this should come transitively
-      debugCompile 'org.aspectj:aspectjrt:1.8.6'
-      compile 'com.jakewharton.hugo:hugo-annotations:1.2.2-SNAPSHOT'
-    }
-
     project.extensions.create('hugo', HugoExtension)
 
     variants.all { variant ->
